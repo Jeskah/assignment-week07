5b7c4d46a9b62cf0c11f112e58587a305d2563ca
Jessica <jessica.antoinette@outlook.com>
2026-02-26 20:11:32 +0000

    altered src code

--- client/src/pages/Genres.jsx
+++ client/src/pages/Genres.jsx
@@ -1,10 +1,16 @@
-// import { useEffect, useState } from 'react';
+// import { useEffect, useState } from "react"
 // import { Link, useParams } from "react-router-dom";
+
 
 export default function Genres() {
     return (
+    <div>
         <div>
-        <h1>GENRES</h1>
+            <h1>So all it took was altering my source code?</h1>
         </div>
+        <div>
+            <h1>GENRES</h1>
+        </div>
+    </div>
     )
 }
